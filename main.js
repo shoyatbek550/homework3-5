7dd5ca886703b355ac4ac7be44ada85409106da3
shoyatbek550 <muhammadayupov550@gmail.com>
2023-05-07 01:41:06 +0500

    hometask 3

---
+++ main.js
@@ -0,0 +1,15 @@
+let yourName = prompt("what is your name");
+let yourLastName = prompt("what is your last name");
+let yourAge = +prompt("what is your age ");
+let yourAddress = prompt("what is your address");
+
+const info = {
+  name: yourName,
+  lastName: yourLastName,
+  age: yourAge,
+  address: yourAddress,
+};
+
+
+console.log(info);
+
